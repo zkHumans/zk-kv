95c625632e6a75e3fda4204c2e8d740ddac86660
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-01 11:33:35 -0700

    chore(jest.preset): print coverage to console

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,10 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+
+  // print coverage to console
+  // https://github.com/nrwl/nx/issues/1337
+  // Note: 202304: causes "Validation Warning" but works
+  coverageReporters: ['html', 'text'],
+};
