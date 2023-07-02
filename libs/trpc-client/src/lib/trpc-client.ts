d9c6f8363a0bacc5e1c9d664c2e4808a5e59efc7
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-02 10:31:46 -0700

    feat(trpc-client): add `API_URL` env aware trpc client
    When creating trpc client, the API URL may be set explicitly
    or it will use env API_URL if availble, and default to '/api'

--- libs/trpc-client/src/lib/trpc-client.ts
+++ libs/trpc-client/src/lib/trpc-client.ts
@@ -1,3 +1,42 @@
-export function trpcClient(): string {
-  return 'trpc-client';
-}
+import SuperJSON from 'superjson';
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
+
+import type { AppRouter } from '@zk-kv/trpc';
+
+// Note: With the exception of NODE_ENV, process.env variables are not
+// available on statically build client, only server-side where there is an
+// actual running process[1]. The `trpc` export will use the url from
+// `process.env['API_URL']` when available and default to '/api' when not.
+//
+// Use `createTRPCClient` to init trpc client with an explictly set api url.
+//
+// [1] https://github.com/remix-run/remix/discussions/2928
+
+const getURL = () => {
+  const urlDefault = '/api';
+  try {
+    return process.env['API_URL'] ?? urlDefault;
+  } catch (e) {
+    return urlDefault;
+  }
+};
+
+export const createTRPCClient = (url = getURL()) =>
+  createTRPCProxyClient<AppRouter>({
+    transformer: SuperJSON,
+    links: [
+      // Log to console in development and only log errors in production
+      // https://trpc.io/docs/links/loggerLink
+      loggerLink({
+        enabled: (opts) =>
+          (process.env['NODE_ENV'] === 'development' &&
+            typeof window !== 'undefined') ||
+          (opts.direction === 'down' && opts.result instanceof Error),
+      }),
+      httpBatchLink({
+        url,
+      }),
+    ],
+  });
+
+export const trpc = createTRPCClient();
