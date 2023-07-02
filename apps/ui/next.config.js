76fc804433abbc6ed94cbdd3e23f1dbf0a3f6921
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-01 17:18:02 -0700

    chore(ui): integrate zkapp-cli generated next.config
    only headers are required, nx finds the modules

--- apps/ui/next.config.js
+++ apps/ui/next.config.js
@@ -12,6 +12,28 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+
+  reactStrictMode: false,
+
+  // To enable SnarkyJS for the web, we must set the COOP and COEP headers.
+  // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const plugins = [
