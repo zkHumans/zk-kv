7ce214e36f094485db700e151e8e59f068afcb14
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-02 07:58:48 -0700

    feat(api(-e2e)): prefix env HOST/PORT with `API_`, default port 3001

--- apps/api-e2e/src/support/test-setup.ts
+++ apps/api-e2e/src/support/test-setup.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 module.exports = async function () {
   // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
+  const host = process.env.API_HOST ?? 'localhost';
+  const port = process.env.API_PORT ?? '3001';
   axios.defaults.baseURL = `http://${host}:${port}`;
 };
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env.API_HOST ?? 'localhost';
+const port = process.env.API_PORT ? Number(process.env.API_PORT) : 3001;
 
 const app = express();
 
