4a17aae27ab572527cffdda7484a1a8a8a8c0d3c
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-02 08:51:07 -0700

    feat(api): serve trpc under `/api/`

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import { expressHandleTRPCRequest } from '@zk-kv/trpc';
 
 const host = process.env.API_HOST ?? 'localhost';
 const port = process.env.API_PORT ? Number(process.env.API_PORT) : 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send({ message: 'Hello API' });
 });
+
+app.use('/api', expressHandleTRPCRequest());
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
