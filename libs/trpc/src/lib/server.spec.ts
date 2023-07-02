3aa05f05565a0c72e0a0b5e3fd7a7a0289c7224d
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-01 22:52:34 -0700

    feat(trpc): add server with auth context and superjson transformer

---
+++ libs/trpc/src/lib/server.spec.ts
@@ -0,0 +1,7 @@
+import { t } from './server';
+
+describe('trpc server', () => {
+  it('should be defined', () => {
+    expect(t).toBeDefined();
+  });
+});
--- libs/trpc/src/lib/trpc.spec.ts
+++
@@ -1,7 +0,0 @@
-import { trpc } from './trpc';
-
-describe('trpc', () => {
-  it('should work', () => {
-    expect(trpc()).toEqual('trpc');
-  });
-});
