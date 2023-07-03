5be6fc710a2b82be04a4bfadb45a2b3a7246bdb4
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-02 21:18:49 -0700

    feat(utils): add functions: delay strToBool displayAccount
    with tests

--- libs/utils/src/lib/utils.spec.ts
+++ libs/utils/src/lib/utils.spec.ts
@@ -1,7 +1,18 @@
-import { utils } from './utils';
+import { displayAccount, strToBool } from './utils';
 
 describe('utils', () => {
-  it('should work', () => {
-    expect(utils()).toEqual('utils');
+  it('strToBool', () => {
+    expect(strToBool('0')).toEqual(false);
+    expect(strToBool('1')).toEqual(true);
+    expect(strToBool('ON')).toEqual(true);
+    expect(strToBool('foo')).toEqual(false);
+    expect(strToBool('true')).toEqual(true);
+    expect(strToBool(undefined)).toEqual(undefined);
+  });
+
+  it('displayAccount', () => {
+    expect(
+      displayAccount('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    ).toEqual('XXXXXX...XXXX');
   });
 });
--- libs/utils/src/lib/utils.ts
+++ libs/utils/src/lib/utils.ts
@@ -1,3 +1,10 @@
-export function utils(): string {
-  return 'utils';
+export const delay = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+// https://stackoverflow.com/a/264180
+export function strToBool(s: string | undefined): boolean | undefined {
+  return s === undefined ? undefined : RegExp(/^\s*(true|1|on)\s*$/i).test(s);
 }
+
+// Show first 6 and last 4 characters of user's Mina account.
+export const displayAccount = (account: string) =>
+  `${account.slice(0, 6)}...${account.slice(-4)}`;
