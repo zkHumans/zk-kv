538b51fb466fc7bc777bd7ac3643431797b3e9b5
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-03 19:32:32 -0700

    feat(api): enable CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { expressHandleTRPCRequest } from '@zk-kv/trpc';
 
 const host = process.env.API_HOST ?? 'localhost';
 const port = process.env.API_PORT ? Number(process.env.API_PORT) : 3001;
 
 const app = express();
+
+// CORS
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (_req, res) => {
   res.send({ message: 'Hello API' });
